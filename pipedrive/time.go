2aba3aaaa40c453a21d54cd672df89560cacabb6
Benjamin Krill <benjamin@krll.de>
2020-03-02 11:33:29 +0100

    empty string on time not set

--- pipedrive/time.go
+++ pipedrive/time.go
@@ -8,13 +8,22 @@ type Timestamp struct {
 }
 
 func (t Timestamp) String() string {
+	if t.Time.IsZero() {
+		return ""
+	}
 	return t.Time.String()
 }
 
 func (t Timestamp) Format() string {
+	if t.Time.IsZero() {
+		return ""
+	}
 	return t.Time.Format("2006-01-02")
 }
 
 func (t Timestamp) FormatFull() string {
+	if t.Time.IsZero() {
+		return ""
+	}
 	return t.Time.Format("2006-01-02 15:04:05")
 }
